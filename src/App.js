5dc26f3db75308c31637b82b342837f6d4875fce
Tony Lin <tonylinswe@gmail.com>
2022-05-28 16:47:01 -0400

    fin hero section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      Template
       <HeroSection />
       <ServicesSection />
       <FooterSection />
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,5 +1,14 @@
 function HeroSection() {
-  return <div className="hero-section">Home</div>;
+  return (
+    <div className="hero-section">
+      <nav>
+        <p>Home</p>
+        <p>Our Services</p>
+        <p>Subscribe</p>
+      </nav>
+      <h2>We do Websites Right</h2>
+    </div>
+  );
 }
 
 export default HeroSection;
